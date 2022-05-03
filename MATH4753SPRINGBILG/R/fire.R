84b3608e1e7eeb8f8e483a1f6fccdf7c4e6cffec
EliBil <98038718+EliBil@users.noreply.github.com>
2022-05-02 23:09:08 -0500

    Add files via upload

---
+++ MATH4753SPRINGBILG/R/fire.R
@@ -0,0 +1,12 @@
+#' fire distance and damage.
+#'
+#' 15 lines of distance and damage
+#'
+#' @format A data frame with 15 rows and 2 variables
+#' \describe{
+#'   \item{DISTANCE}{measure of distance}
+#'   \item{DAMAGES}{scale of damage}
+#'   ...
+#' }
+#' @source canvas
+"fire"
---
+++ MATH4753SPRINGBILG/R/mycltb.R
@@ -0,0 +1,33 @@
+#' @Title mycltb
+#'
+#' @param n number of data points
+#' @param iter number of iterations
+#' @param p probability
+#'
+#'
+#' @return histrogram
+#' @export
+#'
+#' @examples
+mycltb=function(n,iter,p=0.5){
+
+
+  y=rbinom(n*iter,size=n,prob=p)
+
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE)
+
+  w=apply(data,2,mean)
+
+  param=hist(w,plot=FALSE)
+
+  ymax=max(param$density)
+
+  ymax=1.1*ymax
+
+  hist(w,freq=FALSE,  ylim=c(0,ymax),
+       main=paste("Histogram of sample mean","\n", "sample size= ",n,sep=""),
+       xlab="Sample mean")
+
+  curve(dnorm(x,mean=n*p,sd=sqrt(p*(1-p))),add=TRUE,col="Red",lty=2,lwd=3)
+
+}
